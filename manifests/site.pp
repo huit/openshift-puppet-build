0b6c286463eeaf4b728d0dcfb5fdbd13094b81e5
Rob Parrott <robparrott@gmail.com>
2013-06-18 22:50:07 -0400

    refactoring based on origin docs

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,23 +1,48 @@
 node default {
 
-  class { 'openshift_origin' :
-    node_fqdn                  => "${hostname}.${domain}",
-    cloud_domain               => 'openshift.local',
-    dns_servers                => ['8.8.8.8'],
-    os_unmanaged_users         => [],
-    enable_network_services    => true,
-    configure_firewall         => true,
-    configure_ntp              => true,
-    configure_activemq         => true,
-    configure_mongodb          => true,
-    configure_named            => false,
-    configure_avahi            => true,
-    configure_broker           => true,
-    configure_node             => true,
-    development_mode           => true,
-    update_network_dns_servers => false,
-    avahi_ipaddress            => '127.0.0.1',
-    broker_dns_plugin          => 'avahi',
+class { 'openshift_origin' :
+  
+  #The DNS resolvable hostname of this host
+  node_fqdn                  => "${hostname}.${domain}",
+
+  #The domain under which application should be created. Eg: <app>-<namespace>.example.com
+  cloud_domain               => 'example.com',
+
+  #Set to `'nightlies'` to pull from latest nightly build
+  #Or pass path of your locally built source `'file:///root/origin-rpms'`
+  install_repo               => 'nightlies',
+
+  #Upstream DNS server.
+  dns_servers                => ['8.8.8.8'],
+
+  enable_network_services    => true,
+  configure_firewall         => true,
+  configure_ntp              => true,
+
+  #Configure the required services
+  configure_activemq         => true,
+  configure_mongodb          => true,
+  configure_named            => true,
+  configure_avahi            => false,
+  configure_broker           => true,
+
+  #Don't configure the node
+  configure_node             => false,
+
+  #Enable development mode for more verbose logs
+  development_mode           => true,
+
+  #Update the nameserver on this host to point at Bind server
+  update_network_dns_servers => true,
+
+  #Use the nsupdate broker plugin to register application
+  broker_dns_plugin          => 'nsupdate',
+
+  #If installing from a local build, specify the path for Origin RPMs
+  #install_repo               => 'file:///root/origin-rpms',
+
+  #If using BIND, let the broker know what TSIG key to use
+  named_tsig_priv_key         => "${tsig_key}"
   }
 
 }
